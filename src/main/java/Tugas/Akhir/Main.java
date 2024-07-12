48294626e3c5dbfbc20bde015ef6f338153fc184
juntralala <ujun.muhammadjunaidi@gmail.com>
2024-07-12 18:39:45 +0800

    update: override toString() method in Tugas.Akhir.Programmer class

--- src/main/java/Tugas/Akhir/Main.java
+++ src/main/java/Tugas/Akhir/Main.java
@@ -1,9 +1,28 @@
 package Tugas.Akhir;
 
+import java.util.LinkedHashMap;
+
 public class Main {
     public static void main(String[] args) {
         String variableString;
         Pengguna pengguna = new Pengguna();
         Buku buku = new Buku();
+
+        Programmer programmer = new Programmer(1, "UJUN", new String[]{}, "Java", new LinkedHashMap<>());
+
+        programmer.addSkill("Git");
+        programmer.addSkill("Java");
+        programmer.addSkill("PHP");
+        programmer.addSkill("Dart");
+        programmer.addSkill("Javascript");
+        programmer.addSkill("Pascal");
+        programmer.addSkill("MySQL");
+
+        programmer.addSpec("Brand", "Dell");
+        programmer.addSpec("Processor", "Intel I 8365U");
+        programmer.addSpec("RAM", "16GB");
+        programmer.addSpec("SSD", "256GB");
+
+        System.out.println(programmer);
     }
 }
--- src/main/java/Tugas/Akhir/Programmer.java
+++ src/main/java/Tugas/Akhir/Programmer.java
@@ -1,7 +1,6 @@
 package Tugas.Akhir;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Programmer {
     private int id;
@@ -10,8 +9,33 @@ public class Programmer {
     private String pavoritProgrammingLanguage;
     private Map<String, String> laptopSpecs = new LinkedHashMap<>(5);
 
-    public Programmer() {
-        System.out.println("Membuat object Programmer");
+    public Programmer(int id, String name, String[] skils, String pavoritProgrammingLanguage, Map<String, String> laptopSpecs) {
+        this.id = id;
+        this.name = name;
+        this.skils = skils;
+        this.pavoritProgrammingLanguage = pavoritProgrammingLanguage;
+        this.laptopSpecs = laptopSpecs;
+
+        System.out.println("Membuat object Programmer berhasil");
+    }
+
+    public String toString(){
+        return String.format("""
+                {
+                    class: %s,
+                        properties : {
+                        id: %d,
+                        name: %s,
+                        skils: %s,
+                        PavoritProgrammingLanguange: %s,
+                        laptopSpecs: %s
+                    }
+                }""",this.getClass().getName(),
+                    this.id,
+                    this.name,
+                    Arrays.toString(this.skils),
+                    this.getPavoritProgrammingLanguage(),
+                    this.laptopSpecs);
     }
 
     public int getId() {
@@ -52,6 +76,15 @@ public class Programmer {
 
     public String getSpec(String key){
         return this.laptopSpecs.get(key);
+    }
+
+    public void addSkill(String skill){ // ternyata susah bikin operasi kumpulan data pakai array, bukan pake Collection
+        var newSkillSet = new String[this.skils.length + 1];
+        for(int i = 0 ; i < this.skils.length ; i++){
+            newSkillSet[i] = this.skils[i];
+        }
+        newSkillSet[this.skils.length] = skill;
+        this.skils = newSkillSet;
     }
 
     public void removeSpec(String key){
